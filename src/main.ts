b53864623a0b1642ff1eeff86eecdc95aed50274
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-11-05 20:44:53 -0600

    Added some dummy logic. To be replaced very soon

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 //TypeScript Pokedex! A guided project from boot.dev
 //Boy I sure hope it's not a bad time
 
+//Imports go here
 import { startREPL } from "./repl.js";
 
 function main() {
--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,39 @@
+//Imports go here
+
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
+
+// Interfaces and other type info goes here, exported or not
+
+const rl = createInterface({ // Function from Node's readline library
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+});
+
+// Exported functions go here
 export function cleanInput(input: string): string[] {
-    const removedDoubleSpaces = input.replace(/\s\s+/g, " ");
+    // This function takes the user's input and cleans it off of whitespace and splits it into an array of words
+    const removedDoubleSpaces = input.replace(/\s\s+/g, " "); // ngl I have no idea what this regex even means I just grabbed it off of StackOverflow
     const trimmed = removedDoubleSpaces.trim();
     const lower = trimmed.toLowerCase();
     const inputArray = lower.split(" ");
     return inputArray;
+}
+
+export function startREPL(): void {
+    // This function starts the REPL and keeps recursively calling itself (at least somewhat) to constantly prompt the user
+    rl.prompt();
+
+    rl.on("line", (input) => { // Callback function from Node's process library, taking user input after a newline character is detected
+        const cleanedInput = cleanInput(input);
+        // Check if the input is empty
+        if (cleanedInput.length === 0) {
+            rl.prompt()
+        }
+
+        // Print the first word of the command
+        console.log(`Your command was: ${cleanedInput[0]}`);
+        rl.prompt();
+    })
 }
